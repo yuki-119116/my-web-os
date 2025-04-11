3de6a02d0f2c9f140ef55d473acb000bccdeb2c8
yuki-119116 <163005709+yuki-119116@users.noreply.github.com>
2025-04-11 17:48:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,4 +15,41 @@ function dragElement(element)
   var initialY = 0;
   var currentX = 0;
   var currentY = 0;
+
+  if (document.getElementById(element.id + "header"))
+  {
+    document.getElementById(element.id + "header").onmousedown = startDragging;
+  }
+  else
+  {
+    element.onmousedown = startDragging;
+  }
+
+  function startDragging(e)
+    {
+      e = e || window.event;
+      e.preventDefault();
+      initialX = e.clientX;
+      initialY = e.clientY;
+      document.onmouseup = stopDragging;
+      document.onmouseup = dragElement;
+    }
+
+  function dragElement(e)
+    {
+      e = e || window.event;
+      e.preventDefault();
+      currentX = initialX - e.clientX;
+      currentY = initialY - e.clientY;
+      initialX = e.clientX;
+      initialY = e.clientY;
+      element.style.top = (element.offsetTop - currentY) + "px";
+      element.style.left = (element.offsetLeft - currentX) + "px";
+    }
+
+  function stopDragging()
+    {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
 }
